e167d2c1fccf20b2be9cb2db43bc0da128b19fce
GaoJinCheng <gaojc1111@gmail.com>
2025-03-13 00:49:00 +0800

    Site updated: 2025-03-13 00:49:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/13/hello-world/"];function toRandomPost(){
+var posts=["2025/02/18/Learn-It-When-I-Need/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
