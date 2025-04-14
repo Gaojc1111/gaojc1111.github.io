3309f08f12bd4a3df1582ab8b4c2553828cd4e48
GaoJinCheng <gaojc1111@gmail.com>
2025-04-14 11:32:08 +0800

    Site updated: 2025-04-14 11:32:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/18/Learn-It-When-I-Need/","2023/10/11/MySQL笔记/","2024/03/03/SyncMap源码解析/","2023/12/27/欲买桂花同载酒-终不似-少年游/"];function toRandomPost(){
+var posts=["2025/02/18/Learn-It-When-I-Need/","2023/09/24/Linux命令总结/","2024/03/03/SyncMap源码解析/","2023/12/27/欲买桂花同载酒-终不似-少年游/","2023/10/11/MySQL笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
