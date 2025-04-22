789cfb01a4fa54570ba5fc1b24b457f787864001
GaoJinCheng <gaojc1111@gmail.com>
2025-04-22 11:57:50 +0800

    Site updated: 2025-04-22 11:57:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/17/Go的Sync-Pool是什么/","2023/09/24/Linux命令总结/","2025/02/18/Learn-It-When-I-Need/","2025/04/16/Redis-一个靠事件驱动的程序/","2023/10/11/MySQL笔记/","2024/03/03/SyncMap源码解析/","2023/12/27/欲买桂花同载酒-终不似-少年游/","2025/04/21/Redis主从复制/"];function toRandomPost(){
+var posts=["2025/04/17/Go的Sync-Pool是什么/","2023/09/24/Linux命令总结/","2025/02/18/Learn-It-When-I-Need/","2023/10/11/MySQL笔记/","2025/04/16/Redis-一个靠事件驱动的程序/","2025/04/21/Redis主从复制/","2024/03/03/SyncMap源码解析/","2023/12/27/欲买桂花同载酒-终不似-少年游/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
